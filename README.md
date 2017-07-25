ab4ea288d504c508a21d99650ced2540829600dc
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-07-25 11:54:26 -0700

    working version still needs more css

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GifTastic
+
+- Make an array topics to hold my topics
+- Loop through the array to form buttons, a button for each topic
+- when a button is clicked, 10 static gif images of the relative button are displayed.
+
+//javascript, jQuery
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+				
---
+++ jswork.js
@@ -0,0 +1,81 @@
+
+  	var topics = ["dog", "cat" , "rabbit"];
+
+  	var displayButtons = function(){
+
+  		$("#buttons-view").empty();
+  		for (var i = 0; i < topics.length; i++) {
+  			var btn = $("<button>");
+  			btn.text(topics[i]);
+  			btn.addClass("topic");
+  			btn.attr("data-value", topics[i]);
+  			$("#buttons-view").append(btn);
+  		}
+  	}
+
+  	var displayGifs =  function(){
+      
+      $("#gifs-view").empty();
+  		var topic =  $(this).attr("data-value");
+
+  		queryURL = "https://api.giphy.com/v1/gifs/search?q="+ topic +"&api_key=dc6zaTOxFJmzC&limit=10";
+
+  		$.ajax({url: queryURL, method:"GET"})
+  			.done(function(response){
+          var result = response.data;
+
+          for (var i = 0; i < result.length; i++) {
+
+            var gifDiv = $("<div class='item'>"); 
+            var rating = result[i].rating;
+            var pRating = $("<p>").text("Rating: " + rating); 
+  				  var imgStillSrc = result[i].images.fixed_height_still.url;
+            var imgAnitmatedSrc = result[i].images.fixed_height.url;
+
+  				  var img = $("<img class='gifItem'>");
+            img.attr("still-src", imgStillSrc);
+            img.attr("animated-src", imgAnitmatedSrc);
+            img.attr("animate-state", "off");
+            img.attr("src", imgStillSrc);
+            
+
+            gifDiv.append(pRating);
+            gifDiv.append(img);
+  			
+            $("#gifs-view").prepend(gifDiv);
+          }
+
+  			});
+
+  		}
+     
+     var animateOnOff = function(){
+
+        var animateState = $(this).attr("animate-state");
+
+        if(animateState == "off"){
+          
+          var newSrc = $(this).attr("animated-src");
+          $(this).attr("src", newSrc);
+          $(this).attr("animate-state", "on");
+        }
+        else{
+          var newSrc = $(this).attr("still-src");
+          $(this).attr("src", newSrc);
+          $(this).attr("animate-state", "off");
+        }
+     } 
+    
+
+  	$("#submit-topic").on("click", function(){
+  		event.preventDefault();
+  		var newTopic = $("#topic-input").val().trim();
+  		topics.push(newTopic);
+  		displayButtons();
+
+  	});
+
+  	$(document).on("click", ".topic", displayGifs);
+    $(document).on("click", ".gifItem", animateOnOff);
+
+  	displayButtons();
